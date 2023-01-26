a412f4f5836e9d2ba6931d6615cf9b29d50630c9
James Pow <jbpow18@icloud.com>
2023-01-26 18:31:19 -0500

    New php and css to practice Include and Require

--- index.php
+++ index.php
@@ -1,43 +1,25 @@
-<?php
-print_r($_POST);
-print_r($_GET);
-$name = $_GET['name'];
-$name = filter_var($name, FILTER_SANITIZE_SPECIAL_CHARS);
-$email = $_GET['email'];
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
   <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>PHP Practice</title>
+  <meta name="description" content="A simple webpage with a brown header, tan body, and brown footer">
+  <title>Accessible Webpage</title>
   <link rel="stylesheet" href="css/main.css">
 </head>
 
 <body>
-  <h1>Web Form Processor</h1>
-  <form action="<?php echo $_SERVER["PHP_SELF"] ?>" method="get">
-    <label for="name">Name:</label>
-    <input type="text" id="name" name="name"><br><br>
-
-    <label for="email">Email:</label>
-    <input type="email" id="email" name="email"><br><br>
-
-    <label for="subject">Subject:</label>
-    <input type="text" id="subject" name="subject"><br><br>
-
-    <label for="message">Message:</label>
-    <textarea id="message" name="message"></textarea><br><br>
-
-    <div class="buttons">
-      <button type="submit">Submit</button>
-      <button type="submit" formmethod="post">Submit using POST</button>
-      <button type="reset">Reset</button>
-    </div>
-  </form>
+  <header role="banner" class="brown">
+    <h1>Header</h1>
+  </header>
+  <main class="tan">
+    <h2>Body</h2>
+    <p>This is the main content of the webpage. It is placed within a 'main' element to indicate its importance and increase accessibility for screen readers.</p>
+  </main>
+  <footer role="contentinfo" class="brown">
+    <h3>Footer</h3>
+  </footer>
 </body>
 
 </html>
