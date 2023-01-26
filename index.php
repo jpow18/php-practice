9a3a0fc5f6848018cb7c099cc6ae41373fecaad8
James Pow <jbpow18@icloud.com>
2023-01-26 08:34:22 -0500

    First commit.

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP Conditionals</title>
+  <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+  <?php
+    $name = "James";
+  ?>
+  <h1>
+    <?php echo "Hello, {$name}" ?>
+  </h1>
+</body>
+</html>
