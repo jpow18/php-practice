8a1380736314af5e1a6300cea3eaf2688142f87b
James Pow <jbpow18@icloud.com>
2023-02-07 13:38:15 -0500

    Constructor/Destructor.

--- index.php
+++ index.php
@@ -6,14 +6,25 @@
     public $email;
     public  $balance;
 
-    public function getCustomer($id) {
-      $this->id = $id;
-      return 'John Doe';
+    public function __construct($id, $name, $email, $balance){
+    $this->id = $id;
+    $this->name = $name;
+    $this->email = $email;
+    $this->balance = $balance;
+
+
     }
+    public function __destruct()
+    {
+      echo "The destructor ran....";
+    }
+
+    public function getCustomerName() {
+      return $this->name;
+    }
+
   }
 
-  $customer = new Customer;
-
-  echo $customer->id;
-
+  $customer = new Customer(1, "Jonathan", "sdalkfjhas@asdhfa.asd", 12093123);
+  echo $customer->getCustomerName();
 ?>
